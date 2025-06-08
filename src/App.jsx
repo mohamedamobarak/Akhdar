01fa665853015ef2610ac58aef0d15e1e7c29e38
Ahm6dalii <ahmedalielian20@gmail.com>
2025-06-08 20:45:44 +0300

    AA implement i18n and floder structure

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,11 @@ import HowItWorks from './Modules/Home/HowItWorks/HowItWorks'
 import Features from './Modules/Home/Features/Features'
 import Partners from './Modules/Home/Partners/Partners'
 import Footer from './shared/Footer/Footer'
+import { useTranslation } from 'react-i18next';
 
 function App() {
+    const { t } = useTranslation();
+
   return (
     <>
       <Header/>
