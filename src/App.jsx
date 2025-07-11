75b0fdc7d539eda16b6a5c49babe7cc6610e3850
momensale7 <momensaleh2468@gmail.com>
2025-07-11 17:14:59 +0300

    ms-esg

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import AboutUs from './Modules/AboutUs/AboutUs';
 import ImpactPlatform from './Modules/ImpactPlatform/ImpactPlatform';
 import SignUp from './Modules/Auth/Signup/SignUp';
 import Integration from './Modules/Integration/Integration';
+import ESG from './Modules/ESG/ESG';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutUs />} />
+        <Route path="/esg" element={<ESG />} />
         <Route path="/impact-platform" element={<ImpactPlatform />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/api-integration" element={<Integration />} />
---
+++ src/Modules/ESG/ESG.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Hero from './components/Hero'
+import Header from '@/shared/Header/Header'
+
+function ESG() {
+  return (
+    <>
+    <div className="bg-bg">
+      {/* <Header/> */}
+    <div className="container m-auto">
+    <Hero/>
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default ESG
